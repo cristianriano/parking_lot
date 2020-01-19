76bcbb078dc1a1ac42c147acb7ef8cce83f708a8
Cristian Riaño <cristian.riano@runtastic.com>
2020-01-19 22:26:34 +0100

    Add ticket serializer

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'dry-initializer', '~> 3.0'
 gem 'dry-struct', '~> 1.2'
 gem 'dry-types', '~> 1.0'
 
+# Serializer
+gem 'jsonapi-rb', '~> 0.5'
+
 group :test, :development do
   gem 'factory_bot', '~> 5.1'
   gem 'rubocop', '~> 0.76', require: false
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'jsonapi/serializable/renderer'
+require 'serializers/ticket'
+
 # Share helper, common prupose
 module ApplicationHelper
+  SERIALIZER_MAPPING = {
+    'Models::Ticket' => Serializers::Ticket
+  }.freeze
+
+  def renderer
+    @renderer ||= JSONAPI::Serializable::Renderer.new
+  end
 end
